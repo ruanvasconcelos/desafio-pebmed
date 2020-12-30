272d8d99917e63ffa71c97d59d860486a14cf3c8
ruanvasconcelos <ruan.vasconcelos@g.globo>
2020-12-30 17:26:34 -0300

    Move mainactivity para pacote de UI.

--- app/src/main/java/com/example/desafiopebmed/ui/MainActivity.kt
+++ app/src/main/java/com/example/desafiopebmed/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.desafiopebmed
+package com.example.desafiopebmed.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.desafiopebmed.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
